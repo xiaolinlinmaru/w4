01ffba27fab5f4f02aecef6d67acfa1c4c57565b
Xiaolin lin <113568918+xiaolinlinmaru@users.noreply.github.com>
2022-09-29 11:15:40 +0900

    Update main.js

--- w3/main.js
+++ w3/main.js
@@ -15,12 +15,13 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
   //let myName = prompt('Please enter your name.');
   let myName="Rin Kobayashi";
-  if(!myName) {
-    setUserName();
-  } else {
+  //if(!myName) {
+    //setUserName();
+    //myName=promot("Please enter your name");
+ // } else {
     localStorage.setItem('name', myName);
     myHeading.innerHTML = '先週の復習です, ' + myName + 'さん';
-  }
+  //}
 }
 
 if(!localStorage.getItem('name')) {
