d28aa05cdeee94c7ded54ede4d32afd24a2a68ce
Xiaolin lin <113568918+xiaolinlinmaru@users.noreply.github.com>
2022-10-06 11:32:17 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -31,7 +31,7 @@ function result() {
     newStory = newStory.replace('Bob', name);
   }
 
-  if (document.getElementById("").checked) {
+  if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replace('94 fahrenheit', temperature);
